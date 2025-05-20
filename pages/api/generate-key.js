4a85667cf2221a9a4ace5d76027875aa655fd868
H4xScripts <pratikhamal.com@gmail.com>
2025-05-20 20:04:02 +0545

    Update generate-key.js

--- pages/api/generate-key.js
+++ pages/api/generate-key.js
@@ -1,11 +1,8 @@
-// pages/api/generate-key.js
-
 export default function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  // Simple key generator (random 16 characters, letters & numbers)
   const generateKey = () => {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let key = '';
@@ -17,6 +14,5 @@ export default function handler(req, res) {
 
   const newKey = generateKey();
 
-  // For now, just return the key in the response
   res.status(200).json({ key: newKey });
 }
