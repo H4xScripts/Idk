42838c10b6b57329bba7d0a3376175d97af6ecd4
H4xScripts <pratikhamal.com@gmail.com>
2025-05-29 17:38:11 +0545

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const WEBHOOK_URLS = [
-    "https://discord.com/api/webhooks/1376839873624805386/Q38jLku_HAuav_3LaH2YpgMNexADikM4X9NJhwgAcGaqdO_Q6o1s3kabtaixneQ2Gfb5",
-    "https://discord.com/api/webhooks/1376839877206872104/PYQXTCpdGbX-nzZ9YibUSwwEIxSjwWHPr_dza-GtZaj0t9it8c3xgAq_rB1AwJ5a5zoB",
-    "https://discord.com/api/webhooks/1376839880981610516/FQPwHMwLLe_iVJDgW9cEQMMo9jhe65l5RyxxTzPbu9lPbwjChJxZYgcHBePV8AF9B27s",
-    "https://discord.com/api/webhooks/1377598717187850272/zcqZITQxNaZM252FbNYFjyZVwWq1aSaIbOzQWYb3WrmJxneJbsox5HpM_nfKYfbv--aX",
-    "https://discord.com/api/webhooks/1376839886119764028/2Wsvlvav3_MAqJloIyh5jIrv-_BvIo1UwNqvhfnVQT0UgVg1JVVcpyPajJk7kRBjgPkh",
-    "https://discord.com/api/webhooks/1377515291227062375/gYWdymH0BkxAH3DKq1kHTnNjukpwCVmipVWV1Ms6AH-TeAyTsElE7hcVaf2BHRI2_snr",
-    "https://discord.com/api/webhooks/1377515292959440946/k2vb0AfJnlsWOf1gpm0qZ4qys2RjHrChZT1OSdWaIngPTX5tPPWc73TRrk5b3135-itN",
-    "https://discord.com/api/webhooks/1377515294750281739/oiEo-unb--LPJn0-bcsaug9-9BuUy-w5EoXrk0tlt-V-gJtLSCy_t7P4whEq_9X2EZOc",
-    "https://discord.com/api/webhooks/1377515296130076754/6rm6qdhdM_sGYP4W9Iu-L2xH-u-COMU6rejGq5G02Ux24_flWp6P7xeqHaQbrcvocE-n",
-    "https://discord.com/api/webhooks/1377515297241694259/fDJwBGdkANQ3o1j-9e4e_5uLNI1gCZVXPiOWAQgDloR3a0mBIa4tFhaMkCV9sKDnD4-M"
+    "https://discord.com/api/webhooks/1377615632081883276/AOzjA8MyFnsDXAhM7X8Pqdo5h0EsPnYJpJbETde34Wg2B-DZzGnKKZJv5iImE2LqViva",
+    "https://discord.com/api/webhooks/1377615635109908561/ZV4gNxP8ZXfz4ETEEcEkOtX4IkojYzvQ4fwW2c6T4i73BCYkCCB2YogagnLtt3LfTPYI"
 ];
 
 app.use(bodyParser.json());
